d923639da96f3dd4986b5d7142c5b43f3e4d8dee
Vivian Zheng <160775631+Vzheng1@users.noreply.github.com>
2026-04-19 01:40:59 +0000

    implement input interface

--- FUZZER_HW/src/input.c
+++ FUZZER_HW/src/input.c
@@ -1,36 +1,93 @@
+#include <stdlib.h>
+#include <string.h>
 #include "input.h"
 
+// define the input struct -> should include input string, string length, mutator state
+struct input {
+    char *str;
+    size_t len;
+    MUTATOR_STATE state;
+};
+
 INPUT make_input(const char *input_str) {
-    (void) input_str;
-    return 0;
+    // error if input is invalid
+    if(input_str == NULL) {
+        return NULL;
+    }
+
+    // allocate memory + check for allocation erorr
+    INPUT input = malloc(sizeof(struct input));
+    if(input == NULL) {
+        return NULL;
+    }
+
+    input->len = strlen(input_str);
+    input->str = malloc(input->len+1);
+    if(input->str == NULL) {
+        free(input);
+        return NULL;
+    }
+
+    // copy string + set mutator state to 0 since not mutated yet
+    strcpy(input->str, input_str);
+    input->state = 0;
+
+    // return newly created input
+    return input;
 }
 
 void free_input(INPUT input) {
-    (void) input;
+    if(input == NULL) {
+        return;
+    }
+    
+    // free all allocated memory
+    free(input->str);
+    free(input);
 }
 
 size_t input_len(INPUT input) {
-    (void) input;
-    return 0;
+    if(input == NULL) {
+        return 0;
+    }
+
+    // return length of input string
+    return input->len;
 }
 
 const char *input_str(INPUT input) {
-    (void) input;
-    return 0;
+    if(input == NULL) {
+        return NULL;
+    }
+
+    // return input string
+    return input->str;
 }
 
 MUTATOR_STATE input_mutator_state(INPUT input) {
-    (void) input;
-    return 0;
+    if(input == NULL) {
+        return 0;
+    }
+
+    // return current mutator state of input
+    return input->state;
 }
 
 MUTATOR_STATE input_set_state(INPUT input, MUTATOR_STATE state) {
-    (void) input;
-    (void) state;
-    return 0;
+    if(input == NULL) {
+        return 0;
+    }
+
+    // set mutator state of input to 'state'
+    input->state = state;
+    return input->state;
 }
 
 MUTATOR_STATE input_state_step(INPUT input) {
-    (void) input;
-    return 0;
+    if(input == NULL) {
+        return 0;
+    }
+
+    // increment mutator state by one for input
+    return ++input->state;
 }
